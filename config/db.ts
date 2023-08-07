681f33eee7c4e5c05224cd7033b8a72d94b3aea5
Mhammad Nazir <nazirchase@gmail.com>
2023-08-08 00:03:15 +0300

    Added Error handling to connectDB

--- config/db.ts
+++ config/db.ts
@@ -1,6 +1,13 @@
-import { ConnectOptions } from 'mongodb';
 import mongoose from 'mongoose';
 export const connectDB = async () => {
-    await mongoose.connect(process.env.MONGO_URI!);
-    console.log('MongoDb Connected');
+    try {
+        if (!process.env.MONGO_URI) throw "MONGO_URI has not been set! please check your config file!";
+
+        await mongoose.connect(process.env.MONGO_URI!);
+        console.log(`MongoDb Connected`);
+
+    } catch (error) {
+        console.error(`MongoDb Connection failed! Error:\n`, error);
+        process.exit(1);
+    };
 }
