9563433ff22b40dd53b318b577e924dedd52a881
Mhammad Nazir <nazirchase@gmail.com>
2023-08-07 20:54:16 +0300

    Replaced Typegoose model with normal mongoose Schema

--- models/Task.ts
+++ models/Task.ts
@@ -1,16 +1,35 @@
-import { ReturnModelType, prop } from "@typegoose/typegoose";
-import { nanoid } from "nanoid";
+import mongoose, { Schema, model } from "mongoose";
 
-export class Task {
-    @prop({ default: () => nanoid(9) })
-    _id: string;
-
-    @prop()
-    title: string | null;
-
-    @prop()
-    description: string | null;
-
-    @prop({ default: Date.now() })
+export interface ITask extends Document {
+    // _id: mongoose.Schema.Types.ObjectId;
+    title: string;
+    description: string;
     dueDate: Date;
 }
+
+const TaskSchema: Schema = new Schema({
+    // _id: {
+    //     type: mongoose.Schema.Types.ObjectId,
+    //     index: true,
+    //     auto: true
+    // },
+    title: {
+        type: String,
+        required: true,
+        maxLength: [24, "Title cannot be bigger than 24 characters."],
+        index: true
+    },
+    description: {
+        type: String,
+        required: true,
+        maxLength: [1024, "Description cannot be bigger than 255 characters."],
+    },
+    dueDate: {
+        type: Date,
+        required: true,
+        index: true
+    }
+});
+
+// export default model<ITask>("Task", TaskSchema)
+export const Task = model<ITask>("Task", TaskSchema);
