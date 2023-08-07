cf2db2791d073324b01491a82f9d1ce50861022a
Mhammad Nazir <nazirchase@gmail.com>
2023-08-07 20:55:25 +0300

    Added Create to routes + exported router

--- routes/task.ts
+++ routes/task.ts
@@ -1,4 +1,9 @@
 import express from "express";
+import "express-async-errors";
 const router = express.Router();
+const { create } = require("../controllers/task");
 
-router.route(`/create`).post();
+router.route(`/create`).post(create);
+
+
+module.exports = router;
